0a43e7fa99e0e8c7dce96fd0afb933a55c4b4ea2
MD-Kayesur <rmdkayesur@gmail.com>
2026-01-13 13:32:26 +0600

    done create tamplate reac t native app  frontend

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor:"#000000", 
+         
+      }}
+    >
+      <Text style={{ textAlign: "center", color: "#ffffff" }}>Edit app/index.tsx to edit this screen sdfgsdfg </Text>
+    </View>
+  );
+}
